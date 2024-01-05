e5ab8249fa727d0d2c06c67d62c9cc54c65bbe8f
Nicholas Moser <nicholas.r.moser@gmail.com>
2024-01-05 15:50:44 -0700

    updated App.js routes and navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AboutMe from './sections/AboutMe';
 import Portfolio from './sections/Portfolio';
 import Contact from './sections/Contact';
@@ -9,17 +10,20 @@ import Resume from './sections/Resume';
 
 const App = () => {
   return (
+    <Router>
     <div style={{ backgroundColor: '#f0f0f0', minHeight: '100vh' }}>
       <Header />
       <main>
-        <Navigation />
-        <AboutMe />
-        <Portfolio />
-        <Contact />
-        <Resume />
-        {/* Add more components as needed */}
+       <Navigation />
+       <Switch>
+        <Route path="/about" component={AboutMe} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+       </Switch>
       </main>
     </div>
+    </Router>
   );
 };
 
